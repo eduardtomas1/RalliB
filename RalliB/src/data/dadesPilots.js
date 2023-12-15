39f506a596e6790dc28f434fd72ecc649154b985
JuanAn6 <juanantoniorizija666@gmail.com>
2023-12-15 18:00:38 +0100

    classe dia 15

--- RalliB/src/data/dadesPilots.js
+++ RalliB/src/data/dadesPilots.js
@@ -7,25 +7,18 @@ const app = Vue.createApp ({
             pilot_selected : null,
             aposta_selected : null,
             qt_apostada : null,
-            dades : [
-
-            ],
             pilots: [
-                {"nom":"John", "multiplicador": "1.1"},
-                {"nom":"Steve", "multiplicador": "1.7"},
-                {"nom":"Peter", "multiplicador": "3"} //PETER WAS ENDEED HANDLESS
+                {"id":"1", "nom":"John", "multiplicador": "1.1"},
+                {"id":"2", "nom":"Steve", "multiplicador": "1.7"},
+                {"id":"3", "nom":"Peter", "multiplicador": "3"} //PETER WAS ENDEED HANDLESS
             ],
             aposta: [
-                {"nom":"guanyar", "valor":[2.0, 3.0]},
-                {"nom":"podi", "valor":[1.5, 1.9]},
-                {"nom":"posicioExacte", "valor":[2.0, 4.0]},
-                {"nom":"mort", "valor":[4.0, 5.5]}
+                {"id":"1", "nom":"guanyar", "valor":[2.0, 3.0]},
+                {"id":"2", "nom":"podi", "valor":[1.5, 1.9]},
+                {"id":"3", "nom":"posicioExacte", "valor":[2.0, 4.0]},
+                {"id":"4", "nom":"mort", "valor":[4.0, 5.5]}
             ],
-            multiplicador : function () {
-                return {
-
-                }
-            }
+            multiplicador : null,
         }
     },
     
@@ -43,13 +36,58 @@ const app = Vue.createApp ({
         },
         generarAposta ()
         {
-
             if (this.pilot_selected != null && this.aposta_selected != null) {
-                console.log("dins")
+                
+                let p = this.pilots.find(this.findPilot);
+                let a = this.aposta.find(this.findAposta);
+                let arr_multi = this.multiplicador;
+
+                console.log("Pilot: "+p.nom+" aposta: "+ a.nom);
+                let multi = this.randomFloat(a.valor[0],a.valor[1]);
+        
+                let obj = {
+                    "pilot": p.id, "aoposta":a.id, "multi": multi, 
+                }
+
+                
+                //console.log(arr_multi.findIndex(this.findMultiplicador));
+
+                if(this.multiplicador != null){
+                    for(let i = 0; i < this.multiplicador.length; i++){
+                        console.log(this.multiplicador[i]);
+                    }
+                }else{
+                    this.multiplicador = [];
+                    this.multiplicador.push(obj);
+                }
+                console.log(this.multiplicador);
+
+                
+
             }
+        },
+        randomFloat(min, max)
+        {
+            return Math.random()*(max-min)+min;
+        }, 
+        findPilot(e)
+        {
+            return e['id'] == this.pilot_selected;
+        },
+        findAposta(e)
+        {
+            return e['id'] == this.aposta_selected;
+        }, 
+        findMultiplicador(e)
+        {
+            return (e['pilot'] == this.pilot_selected && e['aposta'] == this.aposta_selected);
         }
     }
 
 });
 
 app.mount('#app')
+
+
+//ester ha recomendat fer una funcio de rest i que pasat uns segons es nateji el formulari un cop les dades han estat enviades;
+//si o si use del webStorage
